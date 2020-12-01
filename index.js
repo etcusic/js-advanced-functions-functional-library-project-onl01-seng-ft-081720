eb795513dc9dbbc47410199a4c1e824289a1bac7
Eli Cusic <tntitans31@gmail.com>
2020-12-01 16:49:42 -0600

    commit tree 649988d1fc25e8ef1e0bf8a4d0e399a41fccb861

--- index.js
+++ index.js
@@ -4,16 +4,41 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      if (Object.keys(collection)){
+        let arr = Object.keys(collection)
+        for (let i = 0; i < arr.length; i++){
+          callback(collection[arr[i]])
+        }
+      } else {
+        for (let i = 0; i < collection.length; i++){
+          callback(collection[i])
+        }
+      }
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let newArr = []
+      if (Object.keys(collection)){
+        let arr = Object.keys(collection)
+        for (let i = 0; i < arr.length; i++){
+          newArr[i] = callback(collection[arr[i]])
+        }
+      } else {
+        for (let i = 0; i < collection.length; i++){
+          newArr[i] = callback(collection[i])
+        }
+      }
+        return newArr
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, acc = 0) {
+      // function (acc, val, collection) { return acc + val; }
+      for (let i = 0; i < collection.length; i++){
+        acc = callback(acc, collection[i])
+      }
+      return acc
     },
 
     functions: function() {
